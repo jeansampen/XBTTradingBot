5f79519dd3445cbd1f065543a518f5f57c8a767a
jeansampen <jeansampen93@gmail.com>
2020-04-26 16:49:36 +0200

    hello world dash server exists

---
+++ dash_server.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import time
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from using_plotly_graph_objects import get_figure, update_figure
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+figure = get_figure()
+
+update_figure(figure)
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=figure
+    )
+])
+
+print('Sleeping...')
+time.sleep(3)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- using_plotly_graph_objects.py
+++ using_plotly_graph_objects.py
@@ -1,64 +1,62 @@
-
 import plotly.graph_objects as go
-
 import pandas as pd
 
-print('Loading static_test_data from CSV')
+
 df = pd.read_csv('bitmex_api_data/trade_bucketed.csv')
 
+def get_figure():
+    print('Loading static_test_data from CSV')
 
-print("Creating Figure")
-fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
-                open=df['open'],
-                high=df['high'],
-                low=df['low'],
-                close=df['close'])])
+    print("Creating Figure")
+    fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
+                                         open=df['open'],
+                                         high=df['high'],
+                                         low=df['low'],
+                                         close=df['close'])])
 
-index = 17
-line_offest = 1
-x0 = df.loc[index - line_offest, 'timestamp']
-x1 = df.loc[index + line_offest, 'timestamp']
-y = df.loc[index, 'open']
+    return fig
 
 
-
-
-
-fig.add_trace(
-    go.Scatter(
-        mode='markers',
-        x=[x0],
-        y=[y],
-        marker=dict(
-            symbol='triangle-up',
-            color='LightSkyBlue',
-            size=50,
-            line=dict(
-                color='MediumPurple',
-                width=2
-            )
-        ),
-        showlegend=False
+def update_figure(fig):
+    index = 17
+    line_offest = 1
+    x0 = df.loc[index - line_offest, 'timestamp']
+    x1 = df.loc[index + line_offest, 'timestamp']
+    y = df.loc[index, 'open']
+    fig.add_trace(
+        go.Scatter(
+            mode='markers',
+            x=[x0],
+            y=[y],
+            marker=dict(
+                symbol='triangle-up',
+                color='LightSkyBlue',
+                size=50,
+                line=dict(
+                    color='MediumPurple',
+                    width=2
+                )
+            ),
+            showlegend=False
+        )
     )
-)
 
-fig.update_layout(
-    title='The Great Recession',
-    yaxis_title='AAPL Stock',
-    shapes=[
-        dict(type="line",
-             x0=x0,
-             y0=y,
-             x1=x1,
-             y1=y,
-             line=dict(
-                 color="Black",
-                 width=4,
-                 dash="dashdot",
-             ))],
-)
+    fig.update_layout(
+        title='The Great Recession',
+        yaxis_title='AAPL Stock',
+        shapes=[
+            dict(type="line",
+                 x0=x0,
+                 y0=y,
+                 x1=x1,
+                 y1=y,
+                 line=dict(
+                     color="Black",
+                     width=4,
+                     dash="dashdot",
+                 ))],
+    )
 
-fig.show()
 
 
 
