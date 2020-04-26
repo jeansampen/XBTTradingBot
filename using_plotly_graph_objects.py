ac84a494c9a90afdd93e6bb64675bfc6d14e6e7c
jeansampen <jeansampen93@gmail.com>
2020-04-26 15:47:55 +0200

    We can draw rectangles on a chart

--- using_plotly_graph_objects.py
+++ using_plotly_graph_objects.py
@@ -1,18 +1,29 @@
 import plotly.graph_objects as go
 
 import pandas as pd
-from datetime import datetime
 
 print('Loading static_test_data from CSV')
-df = pd.read_csv('static_test_data/bitstamp.csv')[-1000:]
+df = pd.read_csv('bitmex_api_data/trade_bucketed.csv')
 
 
 print("Creating Figure")
-fig = go.Figure(data=[go.Candlestick(x=df['Timestamp'].map(datetime.fromtimestamp),
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'])])
+fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
 
 print('Showing....')
+
+fig.update_layout(
+    title='The Great Recession',
+    yaxis_title='AAPL Stock',
+    shapes = [dict(
+        x0='2020-04-26T13:10:00.000Z', x1='2020-04-26T13:11:00.000Z', y0=0, y1=1, xref='x', yref='paper',
+        line_width=2)],
+    annotations=[dict(
+        x='2020-04-26T13:10:00.000Z', y=0.05, xref='x', yref='paper',
+        showarrow=False, xanchor='left', text='Increase Period Begins')]
+)
+
 fig.show()
