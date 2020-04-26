92ec167ee610879c1d1a7b3ae443a03c03cf3286
jeansampen <jeansampen93@gmail.com>
2020-04-26 18:47:59 +0200

    It is now possible to print lines

--- simple_trading_algorithm/dash_server.py
+++ simple_trading_algorithm/dash_server.py
@@ -4,16 +4,13 @@ import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from simple_trading_algorithm.plot_figures import get_figure, update_figure
+from simple_trading_algorithm.plot_figures import get_figure
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 figure = get_figure()
-
-update_figure(figure)
-
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
--- simple_trading_algorithm/plot_figures.py
+++ simple_trading_algorithm/plot_figures.py
@@ -4,9 +4,19 @@ import pandas as pd
 
 df = pd.read_csv('bitmex_api_data/trade_bucketed.csv')
 
+
+def get_data_for_index(index):
+    res_x = df.loc[index, 'timestamp']
+    res_y = df.loc[index, selected_column]
+    return [res_x, res_y]
+
+
 selected_column = 'low'
 marker_size = 30
 offset = 10
+line_width = 2
+start_index = get_data_for_index(0)[0]
+end_index = get_data_for_index(999)[0]
 
 
 def get_figure():
@@ -28,10 +38,7 @@ def get_figure():
     return fig
 
 
-def get_data_for_index(index):
-    res_x = df.loc[index, 'timestamp']
-    res_y = df.loc[index, selected_column]
-    return [res_x, res_y]
+
 
 
 def add_sell_triangle_to_figure_for_index(fig, index):
@@ -71,29 +78,34 @@ def add_triangle_to_figure(fig, position_x, position_y, color, direction, size):
         )
     )
 
-def update_figure(fig):
-    index = 17
-    line_offest = 1
-    x0 = df.loc[index - line_offest, 'timestamp']
-    x1 = df.loc[index + line_offest, 'timestamp']
-    y = df.loc[index, selected_column]
-    fig.add_trace(
-        go.Scatter(
-            mode='markers',
-            x=[x0],
-            y=[y],
-            marker=dict(
-                symbol='triangle-up',
-                color='LightSkyBlue',
-                size=50,
-                line=dict(
-                    color='MediumPurple',
-                    width=2
-                )
-            ),
-            showlegend=False
-        )
-    )
+
+def add_buy_line_to_figure(fig, height):
+    add_horizontal_line_to_figure(fig, height, color='Red')
+
+
+def add_sell_line_to_figure(fig, height):
+    add_horizontal_line_to_figure(fig, height, color='Green')
+
+
+
+def add_horizontal_line_to_figure(fig, height, color):
+
+    fig.add_shape(
+        # Line Vertical
+        dict(
+            type="line",
+            x0=start_index,
+            y0=height,
+            x1=end_index,
+            y1=height,
+            line=dict(
+                color=color,
+                width=line_width,
+                dash='dashdot'
+            )
+        ))
+
+
 
 
 index_1 = 17
@@ -102,6 +114,11 @@ index_2 = 100
 figure = get_figure()
 add_buy_triangle_to_figure_for_index(figure, index_1)
 add_sell_triangle_to_figure_for_index(figure, index_2)
+
+add_sell_line_to_figure(figure, 7600)
+add_buy_line_to_figure(figure, 7500)
+
+
 figure.show()
 
 
